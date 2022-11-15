9bd45f2672d6f094123c0f2c439ab04a5281972c
ChristineL367 <60860776+ChristineL367@users.noreply.github.com>
2022-11-15 00:37:31 -0800

    client server putah update

--- client_putah.py
+++ client_putah.py
@@ -5,45 +5,60 @@ import binascii
 import threading
 
 log = []
+class TCP_header():
+    def __init__(self):
+        self.source_prt = 0
+        self.destination_prt = 0
+        self.sequence_num = 0
+        self.ACK_num = 0
+        self.header_length = 0
+        self.unused = 0
+        self.CWR = 0
+        self.ECE = 0
+        self.URG = 0
+        self.ACK = 0  # ack flag 0 if not ack, 1 if syn-ack or ack
+        self.PSH = 0
+        self.RST = 0
+        self.SYN = 0  # syn flag 0 if not syn, 1 if syn-ack or syn
+        self.FIN = 0
+        self.receive_window = 0
+        self.internet_checksum = 0
+        self.urgent_data_ptr = 0
+        self.options = 0
+        self.data = ""
+    def custom_message(self, ack, syn, fin):
 
-def create_message():
-    source_prt = 0
-    destination_prt = 0
-    sequence_num = 0
-    ACK_num = 0
-    header_length = 0
-    unused = 0
-    CWR = 0
-    ECE = 0
-    URG = 0
-    ACK = 0
-    PSH = 0
-    RST = 0
-    SYN = 0
-    FIN = 0
-    receive_window = 0
-    internet_checksum = 0
-    urgent_data_ptr = 0
-    options = 0
-    data = 0
-def send_data_transfer(message):
-    DNS_IP = "169.237.229.88"  # change this by country - currently for the USA
-    DNS_PORT = 53
+        self.ACK = ack #ack flag 0 if not ack, 1 if syn-ack or ack
 
+        self.SYN = syn #syn flag 0 if not syn, 1 if syn-ack or syn
+        self.FIN = fin
+    def get_type(self):
+        if self.ACK == 1 and self.SYN == 1:
+            return "SYN-ACK"
+        elif self.ACK == 1:
+            return "ACK"
+        elif self.SYN == 1:
+            return "SYN"
+        elif self.FIN == 1:
+            return "FIN"
+        elif self.data == "":
+            return "DATA"
+
+def send_synack(message, DNS_IP, DNS_PORT, type, length, client_socket):
     READ_BUFFER = 1024  # size of the buffer to read in the received UDP packet.
 
     address = (DNS_IP, DNS_PORT)
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet, UDP.
+    client = client_socket  # Internet, UDP.
     # send message to given address
 
-    client.sendto(binascii.unhexlify(message), address)
-
+    client.sendto(message, address)
+    logger(53, DNS_PORT, type, length)
     # receive message
 
 
     client.close()
-def send_welcome_port(message): #message can be SYN, SYNACK, ACK, or FIN
+def send_data_port(message): #message can be SYN, SYNACK, ACK, or FIN
     DNS_IP = "169.237.229.88"  # change this by country - currently for the USA
     DNS_PORT = 55 #diff port
 
@@ -59,13 +74,25 @@ def send_welcome_port(message): #message can be SYN, SYNACK, ACK, or FIN
     # receive message
 
     client.close()
-
+def connect():
+    pass
 def logger(source, dest, type, length):
     log.append([source,dest,type,length])
 def print_log():
     for i in log:
         print(i[0], "|", i[1], "|", i[2], "|", i[3])
 if __name__ == '__main__':
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    server_ip = sys.argv[2]
+    server_port = sys.argv[4]
+    DNS_IP = "127.0.0.1"
+    DNS_PORT = 8888 #let os choose port
+    send_synack("ping", DNS_IP, DNS_PORT, "SYN", 0, client_socket)
+    while True:
+        if client_socket.recvfrom(server_port):
+            print(client_socket.recvfrom(server_port))
+            send_data_transfer("ping", DNS_IP, DNS_PORT, "ACK", 0, client_socket)
+            break
+    print_log()
 
-    client_ip = sys.argv[2]
-    port = sys.argv[4]
+
--- server_putah.py
+++ server_putah.py
@@ -1,9 +1,42 @@
 from pickle import FALSE, TRUE
-
+from client_putah import TCP_header
 import sys
+import threading
 import socket
 import binascii
+class TCP_over_UDP(server_ip):
+    def __init__(self):
+        self.connections = [] #queue
+        self.welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.welcome_socket.bind(("127.0.01", 53))
+        self.data_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.data_socket.bind(("127.0.01", 1985))
+        #self.lock = threading.Lock()
+    def thread_handshake(self):
+        t = threading.Thread(target=self.handshake())
+        t.daemon = True
+        t.start()
+    def handshake(self):
+        start = 0
+        packet, address = self.welcome_socket.recvfrom(32) #check this size
+        if packet.get_type() == "SYN" and start == 0:
+            synack = TCP_header()
+            synack.custom_message(1,1,0)
+            send(client_ip, 8888, synack)
+            start+=1
+        packet2, address = self.welcome_socket.recvfrom(32)
+        if packet2.get_type() == "ACK" and start == 1:
+            return 1
+        else:
+            print("Couldn't establish handshake...")
+            return 0
+
+
+    def send(self):
+        pass
+    def receive_data(self):
+        pass
 
 if __name__ == '__main__':
-    server_ip = sys.argv[2]
+    client_ip = sys.argv[2]
     port = sys.argv[4]
